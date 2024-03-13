484afd136b749ccade1f8f90dae40e571eb08e5b
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-03-13 13:05:09 -0400

    Copied Code from CodeHS - Stable

---
+++ main.py
@@ -0,0 +1,190 @@
+"""
+For Clark, it is a normal day, he does his daily routine,
+until an unknown evil strikes, stronger than anything,
+Clark has to embark on a journey to defeat the evil,
+but is not strong enough.
+The only thing that has a chance at stopping it, is  Y O U !
+Try helping clark during the journey to defeat the mystyrious evil
+"""
+import sys
+no = "no"
+yes = "yes"
+
+#start sequence
+print("Hello?")
+print("Ah, good were connected")
+print("Welcome New Player")
+print("in this world players are the most powerful beings")
+playername = input("Now, tell me your name new player, ")
+if playername == "Clark":
+    print("Hmmm,")
+    print("intresting,")
+    print("very, intresting")
+print(playername + ",")
+playerlastname = input(playername + ", tell me you last name ")
+print("now, I will teach you you first battle teckniqe,")
+print(playername + " " + playerlastname + ' Learned how to use the move, "Basic attack" ')
+
+print("remember the spelling and capitals, this is the only time we will tell you this,")
+print("now, you will be connected with Clark, he has sommoned a new player so you will do fine.")
+print("Goodbye")
+pressenter = input(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+
+#meeting clark
+print("Hello?")
+print("Did summoning a player work?")
+diditwork = input("Did it work? ")
+
+while diditwork == "no":
+    print("IT RESPONDED?!, BUT IT SAID NO?!?!?!? SO DID IT WORK?!?!")
+    diditwork = input("Did it work? ")
+
+print("IT WORKED, IT ACUALLY WORKED, WE ARE SAVED, WERE SAVED")
+print("wait, I didint even tell you what is happening")
+print("there is an evil threatining the world, and his castle is only a mile away")
+willyouhelp = input("Will you please help me, in return saving the world ")
+if willyouhelp == "no":
+    print("WHAT!?!? WHY NOT?!?! YOU COULD BE A HERO!!!")
+    print("")
+    print("hello agian,")
+    print("it seems your not yet commited to be a player,")
+    print("come back when you are more commited, " + playername)
+    sys.exit()
+elif willyouhelp == "yes":
+    print("Noice,")
+else:
+    print("I will take that as a yes,")
+print("by the way, what is your name?")
+waittime = input(" ")
+print(playername)
+if playername == "Mr.Beast":
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("MR.BEAST")
+    print("Somehow the enemy exploded due to the mr.beast screams")
+    print("you win")
+    sys.exit()
+
+print("ok " + playername + ", Lets get going")
+
+#enemy encounter
+print("oh no, an enemy blocks the way")
+hp = 2
+print("Enemy's HP: " + str(hp))
+whatdo = input("What will you do? ")
+if whatdo == "Basic attack":
+    print("You defeated the enemy")
+else:
+    print("Not a valid attack, you died")
+    sys.exit()
+print("you made quick work of that guy huh")
+
+#castle found
+print("oh, theres his castle")
+print("im too scared to go in, go in by yourself")
+print("and dont even think about dragging me in")
+willgoin = input("Will you go into the castle? ")
+if willgoin == "yes":
+    print("You walk in to the castle by your own free will")
+else:
+    print('Quit sittin there and thinking, go in! clark pushes you in')
+waittime = input("")
+#in the castle
+print('You see a paper on the floor, it says, "NEW MOVE"')
+print(playername + " " + playerlastname + ' learned the move "Super Awsome Great Attack That Can Oneshot Any Enemy"')
+print("you notice that the ground is preaty bouncy, weird")
+
+print("hello, " + playername)
+print("I bet you're wondering why my palace is a bouncy castle,")
+print("remember me?")
+print("Im the person who brought to here.")
+print("...")
+print("It seems like you are not a speaker")
+print("well it seems like I have no choice but to fight you.")
+print("dark lord challenges you!")
+print("Enemy's HP, 10000000000")
+whatdo = input("Will you use Basic Attack or SAGATCOAE? ")
+if whatdo == "Basic Attack":
+    print("1 point of damage has been dealt,")
+    print("YOU THINK THAT WILL KILL ME")
+    print("Dark lord shot you with SAGATCOAE")
+    print("you died, Game Over")
+    sys.exit()
+elif whatdo == "SAGATCOAE":
+    print("AAAAHHHHHHHHHHHHHHHHHHHHHH")
+else:
+    print("what are you trying to attack me with?")
+    print("die")
+    print("Dark lord shot you with SAGATCOAE")
+    print("you died, Game Over")
+    sys.exit()
+print("Enemy's HP, 0.0000001")
+print("My turn")
+print("Dark lord gets ready to use SAGATCOAE")
+print("Clark comes in the room and kicks the dark lord")
+print("You Win!")
